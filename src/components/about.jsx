3e6aef204eef14f02f30a82ce55a6138757e2584
muhammadmustafaahma <muhammadmustafaahma@airasia.com>
2020-04-06 11:27:44 +0800

    Push all the changes

---
+++ src/components/about.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Me</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a computer science student currently pursuing my Bachelor in Computer Science in UiTM Shah Alam. I love exploring new technologies such as phones and computer parts and often amazed by the progress that we as a human species have made so far in the recent years (if you're asking, I'm in team Red, go AMD!)</p>
+                    <p>I'm a Malaysian and English is not my main language, but I'm trying my best to be able to converse in English. Please forgive me if there're some grammatical mistakes.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What are my skills?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Back-end Development </h3>
+                    <p>I'm confident in my problem solving skills and logics. Hence, I'm confident with my back end programming skills</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Java, C++, Python</h3>
+                    <p>I have intermediate understanding in Java, C++ and Python</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Database Knowledge</h3>
+                    <p>I have some knowledge in managing database such as creating triggers, stored procedures and functions (I mainly use MySQL)</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Airasia <span>2020-present</span></h2>
+                        <p>I recently have joined the Airasia as a Software Developer-Intern in the Enterprise team. My major part of the work has been developing the petty cash system for the staffs in Airasia. I am also exploring new programming techniques such as React, Angular and Golang.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Bachelors in Computer Science (UiTM)<span>2018-present</span></h2>
+                        <p>I am pursuing my Bachelors in Computer Science in Universiti Teknologi MARA (UiTM). I have taken courses like Data Structures, Object-Oriented Programming, Software Development and Web Development over the years and have better understanding of these subjects.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Diploma in Computer Science (UiTM)<span>2015-2018</span></h2>
+                        <p>I have completed my Diploma in Computer Science in Universiti  Teknologi MARA(UiTM). During my time in diploma, I have developed interest in solving logical and mathematical problems which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-6">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2004-2014</span></h2>
+                        <p>I have completed school and achieved 3A5B in my SPM. During my time in school, I gained interests in computer related stuffs and mathematics which had led me to take my diploma in computer science</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
